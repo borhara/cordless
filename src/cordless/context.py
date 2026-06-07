2d633e63355b115990d7e042a516103ff12fff89
borhara <44001656+borhara@users.noreply.github.com>
2026-06-07 17:42:46 +0100

    refactor: reorder channel_id and guild_id properties in Context class

--- src/cordless/context.py
+++ src/cordless/context.py
@@ -8,12 +8,12 @@ class Context:
         return self.interaction.get("member", {}).get("user")
 
     @property
-    def channel_id(self):
-        return self.interaction.get("channel_id")
-
-    @property
     def guild_id(self):
         return self.interaction.get("guild_id")
+
+    @property
+    def channel_id(self):
+        return self.interaction.get("channel_id")
 
     async def send(self, msg):
         return self.responder.send(msg)
