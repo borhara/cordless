b84cd05d2915160f8725c6816e9e31fc506f718f
borhara <44001656+borhara@users.noreply.github.com>
2026-06-07 17:52:30 +0100

    refactor: remove unused user, guild_id, and channel_id properties from Context class

--- src/cordless/context.py
+++ src/cordless/context.py
@@ -3,18 +3,6 @@ class Context:
         self.interaction = interaction
         self.responder = responder
 
-    @property
-    def user(self):
-        return self.interaction.get("member", {}).get("user")
-
-    @property
-    def guild_id(self):
-        return self.interaction.get("guild_id")
-
-    @property
-    def channel_id(self):
-        return self.interaction.get("channel_id")
-
     async def send(self, msg):
         return self.responder.send(msg)
 
