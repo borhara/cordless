faada00ec5222d0aa5f46a1138f2873c5ddd0097
borhara <44001656+borhara@users.noreply.github.com>
2026-07-03 22:33:44 +0100

    feat: export all new public types in __init__

--- src/cordless/__init__.py
+++ src/cordless/__init__.py
@@ -1,17 +1,70 @@
 from .app import Cordless
+from .components import (
+    ActionRow,
+    Button,
+    ButtonStyle,
+    ChannelSelect,
+    Container,
+    MediaGallery,
+    MentionableSelect,
+    Modal,
+    RoleSelect,
+    Section,
+    SelectOption,
+    Separator,
+    StringSelect,
+    TextDisplay,
+    TextInput,
+    TextInputStyle,
+    Thumbnail,
+    UserSelect,
+)
+from .embeds import Embed, EmbedField
 from .errors import (
     CordlessError,
     InvalidSignatureError,
+    NoResponseError,
+    PermissionDeniedError,
     UnknownButtonError,
     UnknownCommandError,
+    UnknownComponentError,
+    UnknownModalError,
     UnsupportedInteractionError,
 )
 
 __all__ = [
     "Cordless",
+    # Components
+    "ActionRow",
+    "Button",
+    "ButtonStyle",
+    "ChannelSelect",
+    "MentionableSelect",
+    "Modal",
+    "RoleSelect",
+    "SelectOption",
+    "StringSelect",
+    "TextInput",
+    "TextInputStyle",
+    "UserSelect",
+    # Embeds
+    "Embed",
+    "EmbedField",
+    # UI Kit
+    "Container",
+    "MediaGallery",
+    "Section",
+    "Separator",
+    "TextDisplay",
+    "Thumbnail",
+    # Errors
     "CordlessError",
     "InvalidSignatureError",
+    "NoResponseError",
+    "PermissionDeniedError",
     "UnknownButtonError",
     "UnknownCommandError",
+    "UnknownComponentError",
+    "UnknownModalError",
     "UnsupportedInteractionError",
 ]
